d8bfcdf586d977dfe46fe76eb4317c9914d6f2ce
Emanuel Ezequiel Meza <emanuelm@fktech.net>
2024-01-28 22:17:45 -0300

    visualizacion de viajes ok

--- src/app/dashboard/viajes/[...slug]/detalle.js
+++ src/app/dashboard/viajes/[...slug]/detalle.js
@@ -8,43 +8,29 @@ import 'moment/locale/es';
 import { API } from "@/utils/constants";
 import { useEffect, useState } from "react";
 import { ApiClient } from "@/utils/apiClient";
+import DatosViaje from "./subcomponents/DatosViaje";
 
 export default function Detalle({ id }) {
-  const apiClient = new ApiClient({ url: API.CHEQUES, id });
-  const [ data, setData ] = useState({});
+  const apiClient = new ApiClient({ url: API.VIAJES, id });
+  const [ data, setData ] = useState(false);
 
   useEffect(() => {
-    apiClient.get({ onSuccess: ({data}) => setData(data) });
+    apiClient.get({ onSuccess: ({data}) => {
+      console.log(data)
+
+      setData({
+        ...data,
+        bitacoras: data.bitacoras.map(({observacion}) => observacion)
+      });
+    }});
   }, []);
 
   return (
-    <Box mt={5} sx={{ display: 'flex', flexDirection: 'column', gap: '.5em' }}>
-      <Typography variant="h5" textAlign={'center'}>
-        Fecha de Emisión: 
-        {data?.fecha_emision ? `${moment(data?.fecha_emision, 'YYYY-MM-DD').format('LL')}` : 'Cargando...'}
-      </Typography>
-      <Typography variant="h5" textAlign={'center'}>
-        Fecha de Emisión: 
-        {data?.fecha_pago ? `${moment(data?.fecha_pago, 'YYYY-MM-DD').format('LL')}` : 'Cargando...'}
-      </Typography>
-      <Typography variant="h5" textAlign={'center'}>
-        Numero: {data?.numero || 'Cargando...'}
-      </Typography>
-      <Typography variant="h5" textAlign={'center'}>
-        Banco: {data?.banco || 'Cargando...'}
-      </Typography>
-      <Typography variant="h5" textAlign={'center'}>
-        Importe: {data?.importe_formateado || 'Cargando...'}
-      </Typography>
-      <Typography variant="h5" textAlign={'center'}>
-        Referencia: {data?.referencia || 'Cargando...'}
-      </Typography>
-      <Typography variant="h5" textAlign={'center'}>
-        Proveedor: {data?.proveedor || 'Cargando...'}
-      </Typography>
-      <Typography variant="h5" textAlign={'center'}>
-        Estado: {data?.estado || 'Cargando...'}
-      </Typography>
+    <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '3em' }}>
+      {data 
+        ? <DatosViaje dataViaje={data} />
+        : <p>{'Cargando...'}</p>
+      }
     </Box>
   )
 }
